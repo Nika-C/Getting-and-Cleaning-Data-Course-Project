eb13cfb403eade07862c795da49c8660a1daea68
Nika-C <chungmk91@hotmail.com>
2018-01-23 13:44:56 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,10 @@
-#Getting and Cleaning Data Course Project
-#Peer-graded Assignment
-
-##==================================================================================================================================
-#check/install and load packaged used in the code
+# check/install and load packaged used in the code
 if("plyr" %in% rownames(installed.packages())==FALSE){install.packages("plyr")}
 if("reshape2" %in% rownames(installed.packages())==FALSE){install.packages("reshape2")}
 library(plyr)
 library(reshape2)
 
-##==================================================================================================================================
+##===================================================================================================================
 ## 1. Merge the training and the test sets to create one data set.
 
 #download data, unzip and set working directory into unzipped folder.
@@ -30,25 +26,25 @@ TrainData <- cbind(SetType=rep("train",nrow(SetTrain)), SubjTrain, LabelTrain, S
 #merge test and tain datasets into one data frame.
 MergedData <- rbind(TestData, TrainData)
 
-##==================================================================================================================================
+##===================================================================================================================
 ## 4. Appropriately labels the data set with descriptive variable names.
 feature <- read.table("features.txt", stringsAsFactors=FALSE)[,2]
 names(MergedData)[-(1:3)] <- feature
 
+##===================================================================================================================
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 meanindex <- grep("mean()",names(MergedData),fixed=TRUE)
 stdindex <- grep("std()",names(MergedData),fixed=TRUE)
 index <- append(meanindex, stdindex)
 SubsetData <- MergedData[,c(1:3,index)]
 
-##==================================================================================================================================
+##===================================================================================================================
 ## 3. Uses descriptive activity names to name the activities in the data set
 activitylabel <- read.table("./activity_labels.txt",stringsAsFactors=FALSE)
 SubsetData$Activity <- activitylabel[SubsetData$Activity,2]
 
-##==================================================================================================================================
-## 5. From the data set in step 4, creates a second, independent tidy data set
-##    with the average of each variable for each activity and each subject.
+##===================================================================================================================
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 MeltedData <- melt(SubsetData, id=c("SetType","Subject","Activity"))
 TidyData <- dcast(MeltedData, SetType+Subject+Activity ~ variable, mean)
 View(TidyData)
